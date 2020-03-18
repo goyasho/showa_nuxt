d7e529742476c4a1855c42559c9244c7c739f497
呉屋 翔大 <goyasho0831@gmail.com>
2020-03-19 00:36:06 +0900

    page

---
+++ nuxt_app/pages/p/_id/_pass.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p v-html="message">no message.</p>
+    <hr>
+    <div class="right">
+      <router-link to="/">Go to Top</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    return (params.id && params.pass);
+  },
+  data: function () {
+    return {
+      title: 'Login'
+    }
+  },
+  computed: {
+    message: function () {
+      const id = this.$route.params.id || '*** no id ***';
+      const pass = this.$route.params.pass || '*** no password ***';
+      return `ID: ${id}<br>PASS: ${pass}`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+a {
+  font-size: 16pt;
+}
+.right {
+  text-align: right;
+}
+</style>
